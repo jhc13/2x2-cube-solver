613a3a46b6503fded636e8942a62b5f2e3c75bad
JC <39209141+jhc13@users.noreply.github.com>
2022-05-01 19:49:47 +0900

    Implement get_observation

--- cube_env.py
+++ cube_env.py
@@ -1,7 +1,17 @@
+import numpy as np
 import gym
 from gym import spaces
 
 from cube import Cube
+
+
+def one_hot_encode(array, category_count):
+    """
+    One-hot encode a 1D array of integers.
+
+    Returns a 2D array.
+    """
+    return np.identity(category_count)[array]
 
 
 class CubeEnv(gym.Env):
@@ -23,7 +33,20 @@ class CubeEnv(gym.Env):
         }
 
     def get_observation(self):
-        pass
+        # The 0 piece is always solved because only U, F, and R moves are made.
+        # The state of the 1 piece can be deduced from the states of the 2 to 7
+        # pieces. Therefore, only the last 6 pieces need to be observed.
+
+        # Subtract 1 from the permutation to transform the range of piece
+        # indices from [1, 7] to [0, 6].
+        permutation = self.cube.permutation.flatten()[2:] - 1
+        orientation = self.cube.orientation.flatten()[2:]
+        encoded_permutation = one_hot_encode(permutation, category_count=7)
+        encoded_orientation = one_hot_encode(orientation, category_count=3)
+        cube = np.hstack((encoded_permutation, encoded_orientation))
+        action_mask = np.ones(6)
+        observation = {'cube': cube, 'action_mask': action_mask}
+        return observation
 
     def reset(self, seed=None, return_info=False, options=None):
         super().reset(seed=seed)
@@ -33,7 +56,7 @@ class CubeEnv(gym.Env):
         info = None
         return (observation, info) if return_info else observation
 
-    def step(self, action):
+    def step(self, action: int):
         move = self.moves[action]
         self.cube.apply_move(move)
         done = self.cube.is_solved()

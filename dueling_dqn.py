24c91bdea30224d99c377133575342755f9b4540
JC <39209141+jhc13@users.noreply.github.com>
2022-05-01 23:36:05 +0900

    Add dueling_dqn.py

---
+++ dueling_dqn.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DuelingDQN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_1 = nn.Linear(60, 256)
+        self.batch_norm_1 = nn.BatchNorm1d(256)
+        self.linear_2 = nn.Linear(256, 128)
+        self.batch_norm_2 = nn.BatchNorm1d(128)
+        self.linear_3 = nn.Linear(128, 64)
+        self.batch_norm_3 = nn.BatchNorm1d(64)
+        self.output_1 = nn.Linear(64, 1)
+        self.output_2 = nn.Linear(64, 6)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = self.batch_norm_1(F.elu(self.linear_1(x)))
+        x = self.batch_norm_2(F.elu(self.linear_2(x)))
+        x = self.batch_norm_3(F.elu(self.linear_3(x)))
+        state_values = self.output_1(x)
+        raw_advantages = self.output_2(x)
+        advantages = (raw_advantages
+                      - raw_advantages.max(dim=1, keepdim=True).values)
+        q_values = state_values + advantages
+        return q_values
